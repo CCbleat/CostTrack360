717b00ba5b3e1bf529ef376d3a60ad4cf2cf10da
人菜瘾大 <ccxiaoy@hotmail.com>
2024-04-22 23:33:14 +0800

    fix: fix the rerender issue

--- app/addPage.tsx
+++ app/addPage.tsx
@@ -37,7 +37,12 @@ export default function addPage() {
             broughtInConsumableNum: broughtInConsumableNum,
         }
         addNewProduct(newProduct);
-        onPressBackBtnAction();
+        // Due to the asynchronous nature of the addNewProduct function, 
+        // the following code is executed after the addNewProduct function is executed
+        // Tackle the refresh issue
+        setTimeout(() => {
+            router.push('/');
+        }, 10);
         console.log('New product added!');
         console.log(newProduct);
     }

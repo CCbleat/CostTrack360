e545d61d18d5025068edfac2abdc2bb1d2365f00
人菜瘾大 <ccxiaoy@hotmail.com>
2024-04-23 18:40:08 +0800

    feat: finish edit function

--- src/components/PagesComponents/AddEditPage/index.tsx
+++ src/components/PagesComponents/AddEditPage/index.tsx
@@ -8,7 +8,7 @@ import { View, ScrollView } from 'tamagui';
 import { PlusRoundedButton, BackRoundedButton, SaveRoundedButton } from '../../buttons/roundedButtons';
 import TopHeadingText from '../../texts/TopHeadingText';
 import AddNewProductInputLine from '../../PagesComponents/AddEditPage/AddNewProductInputLine';
-import { addNewProduct, getAllProducts, getSelectedProductName, storeSelectedProductName } from '../../../tools/SecureStore';
+import { addNewProduct, deleteProduct, getAllProducts, getSelectedProductName, storeSelectedProductName } from '../../../tools/SecureStore';
 import type { newProduct } from '../../../types/NewProductT';
 
 export default function AddEditPage({ isEditPage }: { isEditPage: boolean }) {
@@ -56,9 +56,24 @@ export default function AddEditPage({ isEditPage }: { isEditPage: boolean }) {
     }
 
     const onPressSaveBtnAction = () => {
+        // delete the old product
+        const selectedProductName = getSelectedProductName();
+        deleteProduct(selectedProductName);
         // Save the product
+        const newProduct: newProduct = {
+            productName: productName,
+            currencySign: currencySign,
+            productPrice: productPrice,
+            unitConsumableTime: unitConsumableTime,
+            unitConsumablePrice: unitConsumablePrice,
+            estimatedProductTime: estimatedProductTime,
+            broughtInConsumableNum: broughtInConsumableNum,
+        }
+        addNewProduct(newProduct);
         // Clean the selected product name
         cleanSelectedProductName();
+        // Jump to homePage
+        router.push('/');
     }
 
     const initializeProductProperties = () => {
@@ -119,14 +134,14 @@ export default function AddEditPage({ isEditPage }: { isEditPage: boolean }) {
                     setLineState={setUnitConsumablePrice}
                 />
                 <AddNewProductInputLine
-                    lineKey="预计使用时间"
-                    lineState={estimatedProductTime}
-                    setLineState={setEstimatedProductTime}
-                />
-                <AddNewProductInputLine
                     lineKey="自带耗材数量"
                     lineState={broughtInConsumableNum}
                     setLineState={setBroughtInConsumableNum}
+                />
+                <AddNewProductInputLine
+                    lineKey="预计使用时间"
+                    lineState={estimatedProductTime}
+                    setLineState={setEstimatedProductTime}
                 />
                 <AddNewProductInputLine
                     lineKey="货币符号"
